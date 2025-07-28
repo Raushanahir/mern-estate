d4f647d242d9de03908ac228d45580aeab475e8b
Raushan Kumar <raushanahir0311@gmail.com>
2025-07-28 10:15:56 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -11,7 +12,10 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+
+app.use(cookieParser());
+
+// app.use(cors());
 
 // MongoDB Connection
 mongoose
