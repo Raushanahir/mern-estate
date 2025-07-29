c50ef80f842d581aeb90d6ee93e0c0b01916ff0e
Raushan Kumar <raushanahir0311@gmail.com>
2025-07-29 18:58:40 +0530

    create search API route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
-import CreateListing from "./pages/CreateListing";
+import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
-import Listing from "../../pages/Listing";
+import Listing from './pages/Listing';
 
-
-function App() {
+export default function App() {
   return (
-    <>
-      <h1 className="text-4xl text-pink-500 text-center mt-10">
-        TAILWIND TEST - If you see this in pink, Tailwind is working!
-      </h1>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/about" element={<About />} />
-          <Route path='/listing/:listingId' element={<Listing />} />
-          <Route element={<PrivateRoute />}>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
-           <Route path='/create-listing' element={<CreateListing />} />
-           <Route
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
             path='/update-listing/:listingId'
             element={<UpdateListing />}
           />
         </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
