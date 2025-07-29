cd199a1ba7ced362c68ca7f478b5244d361c8240
Raushan Kumar <raushanahir0311@gmail.com>
2025-07-29 12:21:50 +0530

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -166,6 +166,8 @@ export default function Profile() {
     }
   };
 
+ 
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -300,7 +302,9 @@ export default function Profile() {
               >
                 Delete
               </button>
-              <button className="text-green-700 uppercase">Edit</button>
+               <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
             </div>
           ))}
         </div>
